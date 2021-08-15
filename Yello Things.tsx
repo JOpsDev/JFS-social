8c89956778660ded6b104808d39fdd299664137e
Tobias Frech <tobias@frech.info>
2021-08-15 21:46:15 +0200

    first commit

---
+++ Yello Things.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Yello Things" tilewidth="32" tileheight="32" tilecount="200" columns="10">
+ <image source="yellow-map.png" width="320" height="640"/>
+ <tile id="67">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
